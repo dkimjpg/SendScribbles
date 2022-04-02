57f58c185cd4d9d7f5785acc4b0d5826ca4d1f0a
Dradeon <69202556+Dradeon@users.noreply.github.com>
2022-04-02 12:05:01 -0400

    Added Fragments and Bottom Navigation to Main Activity

--- app/src/main/java/com/example/sendscribbles/MainActivity.java
+++ app/src/main/java/com/example/sendscribbles/MainActivity.java
@@ -1,22 +1,60 @@
 package com.example.sendscribbles;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.sendscribbles.Fragments.NewPostFragment;
+import com.example.sendscribbles.Fragments.ProfileFragment;
+import com.example.sendscribbles.Fragments.UserFeedFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnLogOut;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private Button btnLogOut;
+    private BottomNavigationView bottomNavigationView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnLogOut = findViewById(R.id.btnLogOut);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch(item.getItemId()){
+                    case R.id.actionHome:
+                        fragment = new UserFeedFragment();
+                        break;
+                    case R.id.actionNewPost:
+                        fragment = new NewPostFragment();
+                        break;
+                    case R.id.actionProfile:
+                        fragment = new ProfileFragment();
+                        break;
+                    default:
+                        fragment = new UserFeedFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.frameLayout,fragment).commit();
+                return true;
+            }
+
+        });
 
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
